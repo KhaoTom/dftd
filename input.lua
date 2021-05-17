2a34e0a090ccddfba32dbd8402aac61f9a83487b
Edwin DeNicholas <disruption999@hotmail.com>
2021-05-16 19:18:40 -0700

    Add more scancodes for movement.

--- input.lua
+++ input.lua
@@ -8,19 +8,19 @@ function handlePlayerMove(scancode)
   outVector = {}
   outVector.x = 0
   outVector.y = 0
-  if scancode == "d" then
+  if scancode == "right" or scancode == "d" or scancode == "l" or scancode == "kp6" then
     outVector.x = 1
     return outVector
   end
-  if scancode == "a" then
+  if scancode == "left" or scancode == "a" or scancode == "h" or scancode == "kp4" then
     outVector.x =  -1
     return outVector
   end
-  if scancode == "w" then
+  if scancode == "up" or scancode == "w" or scancode == "k" or scancode == "kp8" then
     outVector.y = -1
     return outVector
   end
-  if scancode == "s" then
+  if scancode == "down" or scancode == "s" or scancode == "j" or scancode == "kp2" then
     outVector.y = 1
     return outVector
   end
